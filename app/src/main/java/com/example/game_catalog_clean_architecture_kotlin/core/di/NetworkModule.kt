06bf0072c61d5bc26425489567e8ee14414eff1e
diazagfanuribi <diazagfa@gmail.com>
2020-11-26 23:53:53 +0700

    paging source setup

--- app/src/main/java/com/example/game_catalog_clean_architecture_kotlin/core/di/NetworkModule.kt
+++ app/src/main/java/com/example/game_catalog_clean_architecture_kotlin/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -25,6 +26,7 @@ class NetworkModule {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.rawg.io/api/")
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(client)
             .build()
         return retrofit.create(ApiService::class.java)
